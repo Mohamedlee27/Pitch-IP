63b19a6cc7af813a9a71ca7c18fb9da6700c528e
Mohamedlee27 <leonk8070@gmail.com>
2022-05-12 11:25:05 +0300

    created a route function to create a new page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import main 
-
+from flask_login import current_user,login_required
+from ..models import Pitch
+from forms import PitchForm 
 # Views
 @main.route('/')
 def index():
@@ -9,3 +11,18 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('create_pitch.html', form = form)
