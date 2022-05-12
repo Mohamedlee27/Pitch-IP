7877c9772c392b33128104a1c078373fcc12aa5a
Mohamedlee27 <leonk8070@gmail.com>
2022-05-12 10:19:33 +0300

    created all the module classes created instances for db

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+bootstap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+
+def create_app():
+    app = Flask(__name__, debug=True)
+
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstap.init_app(app)
+    
+
+
+    return app
